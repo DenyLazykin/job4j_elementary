b83e92f5dbaddec49427869973202a9cbc98fd6b
Denis Lazykin <dlazykin856@gmail.com>
2022-10-05 18:08:54 +0300

    2. Тесты для задачи "идеальный вес" [#159458 #230713] исправил название тест-метода

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -22,7 +22,7 @@ public class FitTest {
     }
 
     @Test
-    public void whenMan165Then75dot75() {
+    public void whenMan165Then74dot75() {
         short in = 165;
         double expected = 74.75;
         double out = Fit.manWeight(in);

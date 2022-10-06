7ed77511b7fb2bff5b300136beb7a4ac70a47e9c
Denis Lazykin <dlazykin856@gmail.com>
2022-10-06 10:12:13 +0300

    3. Приведение типов. Целые числа

---
+++ src/main/java/ru/job4j/type/IntegerTypeConversion.java
@@ -0,0 +1,10 @@
+package ru.job4j.type;
+
+public class IntegerTypeConversion {
+    public static void main(String[] args) {
+        int num = 1;
+        short numLower = (short) num;
+        System.out.println((long) num);
+        System.out.println(numLower);
+    }
+}

68536affcbe3039a6948bc72c846c97ad96b7568
Denis Lazykin <dlazykin856@gmail.com>
2022-10-10 12:18:18 +0300

    23. Разделить трехзначное число на отдельные цифры и вывести их в обратном порядке

---
+++ src/main/java/ru/job4j/type/Task23.java
@@ -0,0 +1,15 @@
+package ru.job4j.type;
+
+public class Task23 {
+    public static void separate(int number) {
+        int num1, num2, num3;
+        num1 = number / 100;
+        num2 = number % 100 / 10;
+        num3 = number % 100 % 10;
+        System.out.println(num3 + " " + num2 + " " + num1);
+    }
+
+    public static void main(String[] args) {
+        separate(205);
+    }
+}

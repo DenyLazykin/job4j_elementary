f50d9ec1913d3579ae027d4757f5440e75281f77
Denis Lazykin <dlazykin856@gmail.com>
2022-10-11 19:08:41 +0300

    26. Равенство двух чисел

---
+++ src/main/java/ru/job4j/condition/Task26.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Task26 {
+    public static void isEqual(int num1, int num2) {
+        if (num2 == num1) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
+

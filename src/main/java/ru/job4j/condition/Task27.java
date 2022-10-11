0cf65a9b21e1d6131ca3984c3f9f539d8dd92937
Denis Lazykin <dlazykin856@gmail.com>
2022-10-11 19:15:39 +0300

    27. Ученики и стулья

---
+++ src/main/java/ru/job4j/condition/Task27.java
@@ -0,0 +1,8 @@
+package ru.job4j.condition;
+
+public class Task27 {
+    public static void isEnough(int pupils, int chairs) {
+        System.out.println(pupils >= chairs
+                ? "Нет" : "Да");
+    }
+}

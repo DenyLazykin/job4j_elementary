00be9f01e0c5f51a454f5ef8d0c9c9f3064131ba
Denis Lazykin <dlazykin856@gmail.com>
2022-10-11 19:34:53 +0300

    30. Изменить число в зависимости от его знака

---
+++ src/main/java/ru/job4j/condition/Task30.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Task30 {
+    public static void numModify(int number) {
+        if (number < 0) {
+            System.out.println(Math.abs(number));
+        } else {
+            number = 0;
+            System.out.println(number);
+        }
+    }
+
+    public static void main(String[] args) {
+        numModify(-6);
+    }
+}

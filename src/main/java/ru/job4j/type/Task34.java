c2f370a07de770c6965167d29853148b5db3008b
Denis Lazykin <dlazykin856@gmail.com>
2022-10-12 18:20:06 +0300

    34. Найти количество четных цифр в четырехзначном числе

---
+++ src/main/java/ru/job4j/type/Task34.java
@@ -0,0 +1,32 @@
+package ru.job4j.type;
+
+public class Task34 {
+    public static void evenAmount(int number) {
+        int num1, num2, num3, num4, counter = 0;
+        int temp = number / 10;
+        int temp2 = temp / 10;
+
+        num1 = temp2 / 10;
+        num2 = temp2 % 10;
+        num3 = temp % 10;
+        num4 = number % 10;
+
+        if (num1 % 2 == 0) {
+            counter++;
+        }
+        if ((num2 % 2 == 0)) {
+            counter++;
+        }
+        if ((num3 % 2 == 0)) {
+            counter++;
+        }
+        if (num4 % 2 == 0) {
+            counter++;
+        }
+        System.out.println(counter);
+    }
+
+    public static void main(String[] args) {
+        evenAmount(4012);
+    }
+}

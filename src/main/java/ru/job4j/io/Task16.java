4a776a6c8caa8c6e418d5d47c212fb1fab6c432c
Denis Lazykin <dlazykin856@gmail.com>
2022-10-12 11:23:49 +0300

    16. Вывод на консоль надписи "+++!!!!??" (2)

---
+++ src/main/java/ru/job4j/io/Task16.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+public class Task16 {
+    public static void main(String[] args) {
+        String source = "+!?";
+        System.out.print(source.charAt(0));
+        System.out.print(source.charAt(0));
+        System.out.print(source.charAt(0));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(1));
+        System.out.print(source.charAt(2));
+        System.out.print(source.charAt(2));
+    }
+}

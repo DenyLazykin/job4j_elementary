b8f1926b97bd23b452d7f53fb26ef06c57145a1a
Denis Lazykin <dlazykin856@gmail.com>
2022-10-13 11:14:31 +0300

    21. Разделить трехзначное число на отдельные цифры. Исправил формулу разделяющую входящее число на цифры

--- src/main/java/ru/job4j/type/Task21.java
+++ src/main/java/ru/job4j/type/Task21.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task21 {
     public static void separate(int number) {
         int num1, num2, num3;
-        num1 = number % 10;
-        num2 = number / 10 % 10;
-        num3 = number / 10 / 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
         System.out.println(num1 + " " + num2 + " " + num3);
     }
 

239e501a4170cdd86cfc37b900149a26d86b04bf
Denis Lazykin <dlazykin856@gmail.com>
2022-10-13 11:04:04 +0300

    34. Найти количество четных цифр в четырехзначном числе. Исправил формулу разделения входящего числа на цифры

--- src/main/java/ru/job4j/type/Task34.java
+++ src/main/java/ru/job4j/type/Task34.java
@@ -3,13 +3,10 @@ package ru.job4j.type;
 public class Task34 {
     public static void evenAmount(int number) {
         int num1, num2, num3, num4, counter = 0;
-        int temp = number / 10;
-        int temp2 = temp / 10;
-
-        num1 = temp2 / 10;
-        num2 = temp2 % 10;
-        num3 = temp % 10;
-        num4 = number % 10;
+        num1 = number % 10;
+        num2 = number / 10 % 10;
+        num3 = number / 10 / 10 % 10;
+        num4 = number / 10 / 10 / 10;
 
         if (num1 % 2 == 0) {
             counter++;

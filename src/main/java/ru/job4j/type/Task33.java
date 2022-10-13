3fe7020e5722f5d77cf44f03bb4a4f02f3071a8e
Denis Lazykin <dlazykin856@gmail.com>
2022-10-13 11:16:09 +0300

    33. Найти количество четных цифр в трехзначном числе Исправил формулу разделяющую входящее число на цифры

--- src/main/java/ru/job4j/type/Task33.java
+++ src/main/java/ru/job4j/type/Task33.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task33 {
     public static void evenAmount(int number) {
         int num1, num2, num3, counter = 0;
-        num1 = number % 10;
-        num2 = number / 10 % 10;
-        num3 = number / 10 / 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
 
         if (num1 % 2 == 0) {
             counter++;

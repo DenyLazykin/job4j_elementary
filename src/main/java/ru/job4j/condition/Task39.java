e707469bb64f9f8132c150dc458027bcf40ccdbe
Denis Lazykin <dlazykin856@gmail.com>
2022-10-17 13:44:18 +0300

    38. Проверить симметричность трехзначного числа

---
+++ src/main/java/ru/job4j/condition/Task39.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Task39 {
+    public static void isSymmetric(int number) {
+        int num1, num2, num3;
+        int temporary = number / 10;
+
+        num1 = temporary / 10;
+        num2 = temporary % 10;
+        num3 = number % 10;
+        if (num1 == num3) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}

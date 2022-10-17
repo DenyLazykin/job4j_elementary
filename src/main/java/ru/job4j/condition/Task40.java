20bed99ba24b3af604971d52b318773574b59d20
Denis Lazykin <dlazykin856@gmail.com>
2022-10-17 13:57:55 +0300

    40. "Дважды четное" трехзначное число

---
+++ src/main/java/ru/job4j/condition/Task40.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Task40 {
+    public static void isDoubleEven(int number) {
+        int num1, num2, num3;
+        int temporary = number / 10;
+
+        num1 = temporary / 10;
+        num2 = temporary % 10;
+        num3 = number % 10;
+        System.out.println(number % 2 == 0
+                && (num1 + num2 + num3) % 2 == 0
+                ? "Да" : "Нет");
+    }
+}

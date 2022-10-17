d0c4de6405d8b1af497fed2f3d020dceeba6f34e
Denis Lazykin <dlazykin856@gmail.com>
2022-10-17 13:48:37 +0300

    39. Является ли первая цифра двузначного числа больше второй

---
+++ src/main/java/ru/job4j/condition/Task37.java
@@ -0,0 +1,10 @@
+package ru.job4j.condition;
+
+public class Task37 {
+    public static void numsCompare(int number) {
+        int num1, num2;
+        num1 = number / 10;
+        num2 = number % 10;
+        System.out.println(num1 > num2 ? "Да" : "Нет");
+    }
+}

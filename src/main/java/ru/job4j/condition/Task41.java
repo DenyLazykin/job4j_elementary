574e4490c9bd514316ef3192cfcce848277a73b2
Denis Lazykin <dlazykin856@gmail.com>
2022-10-17 14:07:25 +0300

    41. Найти количество четных цифр трехзначного числа

---
+++ src/main/java/ru/job4j/condition/Task41.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+public class Task41 {
+    public static void evenDigitsAmount(int number) {
+        int num1, num2, num3;
+        int temporary = number / 10;
+        int counter = 0;
+
+        num1 = temporary / 10;
+        num2 = temporary % 10;
+        num3 = number % 10;
+
+        if (num1 % 2 == 0) {
+            counter++;
+        }
+        if (num2 % 2 == 0) {
+            counter++;
+        }
+        if (num3 % 2 == 0) {
+            counter++;
+        }
+        System.out.println(counter);
+    }
+
+    public static void main(String[] args) {
+        evenDigitsAmount(320);
+    }
+}

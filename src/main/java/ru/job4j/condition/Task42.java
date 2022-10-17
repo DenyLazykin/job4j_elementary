75d61590acfa947d4df65cd78088c862dce7f45c
Denis Lazykin <dlazykin856@gmail.com>
2022-10-17 14:19:04 +0300

    42 Является ли четырехзначное число "парно-симметричным"

---
+++ src/main/java/ru/job4j/condition/Task42.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Task42 {
+    public static void isPairSymmetric(int number) {
+        int temporary = number / 10;
+        int temporary1 = temporary / 10;
+
+        int num1 = temporary1 / 10;
+        int num2 = temporary1 % 10;
+        int num3 = temporary % 10;
+        int num4 = number % 10;
+        int num1AndNum2 = Integer.parseInt(num1 + "" + num2);
+        int num3AndNum4 = Integer.parseInt(num3 + "" + num4);
+
+        System.out.println(num1AndNum2 == num3AndNum4 ? "Да" : "Нет");
+    }
+}

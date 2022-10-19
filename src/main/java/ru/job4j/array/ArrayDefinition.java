e48c0da45338482d3d7ff8a4bda19bdba0bf2218
Denis Lazykin <dlazykin856@gmail.com>
2022-10-19 18:23:11 +0300

    6.0.4. Массивы и цикл for. [#226665 #236480]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,18 +3,25 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива ages равен: " + ages.length);
 
         String[] surnames = new String[100500];
+        System.out.println("Размер массива surnames равен: " + surnames.length);
 
         String[] names = new String[4];
         names[0] = "Денис";
         names[1] = "Настя";
         names[2] = "Тема";
         names[3] = "Даник";
+        for (int index = 0; index < names.length; index++) {
+            System.out.println(names[index]);
+        }
 
         float[] prices = new float[40];
+        System.out.println("Размер массива prices равен: " + prices.length);
 
         int[] ints = new int[10];
+        System.out.println("Размер массива ints равен: " + ints.length);
 
         int[] nums = new int[5];
         nums[0] = 2006;
@@ -23,11 +30,14 @@ public class ArrayDefinition {
         nums[3] = 2018;
         nums[4] = 2022;
         int years = nums[3];
-        System.out.println("Размер массива ints равен: " + ints.length);
-        System.out.println("Размер массива prices равен: " + prices.length);
-        System.out.println("Размер массива surnames равен: " + surnames.length);
-        System.out.println("Размер массива ages равен: " + ages.length);
         System.out.println(years);
-        System.out.println(names[0] + " " + names[1] + " " + names[2] + " " + names[3]);
+
+        int[] numbers = new int[10];
+        for (int index = 0; index < numbers.length; index++) {
+            numbers[index] = index;
+        }
+        for (int index = 0; index < numbers.length; index++) {
+            System.out.println(numbers[index]);
+        }
     }
 }

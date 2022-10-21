66c5ec0b7bf5ad95f74fd98276ec4c3011d756ca
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 18:30:38 +0300

    29. Посчитать сумму цифр трехзначного числа. Исправил формулу отделения цифр от числа

--- src/main/java/ru/job4j/type/Task29.java
+++ src/main/java/ru/job4j/type/Task29.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task29 {
     public static void numSum(int number) {
         int num1, num2, num3;
-        num1 = number / 100;
-        num2 = number % 100 / 10;
-        num3 = number % 100 % 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
         int rsl = num1 + num2 + num3;
         System.out.println(rsl);
     }

8833dbde5d2720f87ad1c6f0ef41c8ea44ade660
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 12:23:17 +0300

    6.4.0. Слово начинается с ... [#41585 #237099]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -1,6 +1,9 @@
 package ru.job4j.array;
 
 public class ArrayChar {
+    /*
+    Метод проверяет, что слово начинается с определенной последовательности
+     */
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {

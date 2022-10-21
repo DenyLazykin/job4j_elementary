beb78a1384a8fe23d601f8ff9bf98094b95cef8d
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 18:29:48 +0300

    27. Трансформировать трехзначное число. Исправил формулу отделения цифр от числа

--- src/main/java/ru/job4j/type/Task27.java
+++ src/main/java/ru/job4j/type/Task27.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task27 {
     public static void transform(int number) {
         int num1, num2, num3;
-        num1 = number / 100;
-        num2 = number % 100 / 10;
-        num3 = number % 100 % 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
         int rsl = Integer.parseInt(num3 + "" + num2 + "" + num1);
         System.out.println(rsl - 20);
     }

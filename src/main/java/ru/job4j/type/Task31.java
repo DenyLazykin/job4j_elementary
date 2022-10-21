177eab0f0ecc64786d50e20673ec386e3522416a
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 18:31:26 +0300

    31. Скомбинировать значение из цифр трехзначного числа. Исправил формулу отделения цифр от числа

--- src/main/java/ru/job4j/type/Task31.java
+++ src/main/java/ru/job4j/type/Task31.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task31 {
     public static void difference(int number) {
         int num1, num2, num3;
-        num1 = number / 100;
-        num2 = number % 100 / 10;
-        num3 = number % 100 % 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
         int rsl = Math.abs(num1 - num3) * num2;
         System.out.println(rsl);
     }

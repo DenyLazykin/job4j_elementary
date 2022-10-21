d56257cc0f70ad5f8db3a29d373bb768cbf3ce0c
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 18:26:14 +0300

    43. Содержится ли в трехзначном числе хотя бы один ноль

---
+++ src/main/java/ru/job4j/condition/Task43.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Task43 {
+    public static void containsZero(int number) {
+        int temp = number / 10;
+
+        int num1 = temp / 10;
+        int num2 = temp % 10;
+        int num3 = number % 10;
+        System.out.println(num3 == 0
+                || num2 == 0
+                || num1 == 0 ? "Да" : "Нет");
+    }
+}

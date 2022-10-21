16c1c2fe8fba6adcb4099169aa6ef47767ac8b1a
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 13:03:21 +0300

    23. Вывести ряд чисел с последующим увеличением

---
+++ src/main/java/ru/job4j/loop/Task23.java
@@ -0,0 +1,9 @@
+package ru.job4j.loop;
+
+public class Task23 {
+    public static void loop(int num) {
+        for (int i = num + 1; i < num + 6; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}

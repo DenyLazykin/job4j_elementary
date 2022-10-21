bfc552463f22d6eb0cfe80f52078c885336e21fb
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 17:55:43 +0300

    6.4.3. Слово заканчивается на ... [#156317 #237119]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -1,6 +1,9 @@
 package ru.job4j.array;
 
 public class EndsWith {
+    /*
+    Метод проверяет, что слово заканчивается с определенной последовательности
+     */
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {

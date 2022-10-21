a41d3375f0f0515b69fb5d57642943c66b6e9ed7
Denis Lazykin <dlazykin856@gmail.com>
2022-10-21 18:28:38 +0300

    25. Вывести трехзначное число в "полном виде". Исправил формулу отделения цифр от числа

--- src/main/java/ru/job4j/type/Task25.java
+++ src/main/java/ru/job4j/type/Task25.java
@@ -3,9 +3,11 @@ package ru.job4j.type;
 public class Task25 {
     public static void separate(int number) {
         int num1, num2, num3;
-        num1 = (number / 100) * 100;
-        num2 = (number % 100 / 10) * 10;
-        num3 = number % 100 % 10;
+        int temp = number / 10;
+
+        num1 = temp / 10;
+        num2 = temp % 10;
+        num3 = number % 10;
         System.out.println(num1 + "+" + num2 + "+" + num3);
     }
 

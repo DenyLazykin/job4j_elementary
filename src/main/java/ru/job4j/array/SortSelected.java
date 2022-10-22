2e3f1afc7bba42e90b1dc65db6c795a704cf938f
Denis Lazykin <dlazykin856@gmail.com>
2022-10-22 15:48:25 +0300

    6.5.3. Сортировка выборкой. [#156320 #238357]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class SortSelected {
+    /*
+    Метод сортирует массив выборкой.
+    Смысл метода в поиске минимального элемента из осавшихся
+     */
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, i, index);
+        }
+        return data;
+    }
+
+    public static void main(String[] args) {
+        int[] data = new int[]{3, 5, 1};
+        System.out.println(Arrays.toString(sort(data)));
+
+    }
+}

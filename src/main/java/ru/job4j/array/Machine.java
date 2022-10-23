be4a9ec2f9d920bc2a828a7c06593d9272fb8c54
Denis Lazykin <dlazykin856@gmail.com>
2022-10-23 18:37:25 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #239005]

---
+++ src/main/java/ru/job4j/array/Machine.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class Machine {
+    /*
+    Метод реализует механизм возврата монет в вендинговых аппаратах (жадный алгоритм)
+     */
+    public static int[] change(int money, int price) {
+        int[] coins = {10, 5, 2, 1};
+        int[] rsl = new int[100];
+        int size = 0;
+        int cash = money - price;
+        for (int coin : coins) {
+            while (cash >= coin) {
+                cash = cash - coin;
+                rsl[size] = coin;
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}

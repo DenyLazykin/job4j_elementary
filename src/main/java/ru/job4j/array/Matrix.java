91153f827b0f2dae0da28d3120ceaaf62b60620a
Denis Lazykin <dlazykin856@gmail.com>
2022-10-23 15:37:22 +0300

    6.6.4. Двухмерный массив. Таблица умножения. [#33491 #238435]

---
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class Matrix {
+    /*
+    Метод формирует таблицу умножения
+     */
+    public static int[][] multiple(int size) {
+        //создаем двумерный массив, в который будем записывать значения
+        int[][] table = new int[size][size];
+        //цикл для прохождения по каждому элементу массива
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                //записываем значения в созданный массив
+                table[row][cell] = (row + 1) * (cell + 1);
+            }
+        }
+        return table;
+    }
+}

58b6df672370cec00e222b08602fb3ace1556ec3
Denis Lazykin <dlazykin856@gmail.com>
2022-11-10 09:37:57 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]. Переписал код с использованием составных операторов присваивания в классе OperatorsRunner

--- src/main/java/ru/job4j/condition/OperatorsRunner.java
+++ src/main/java/ru/job4j/condition/OperatorsRunner.java
@@ -1,16 +1,21 @@
 package ru.job4j.condition;
 
+/**
+ * мы в коде при использовании составных операторов присваивания
+ * мы часто переназначали переменную first или second – связано это с тем,
+ * что при использовании операторов мы переназначаем значение левой от знака присваивания переменной
+ */
 public class OperatorsRunner {
     public static void main(String[] args) {
         int first = 2;
         int second = 10;
-        int result = first + second;
-        System.out.println("Result sum: " + result);
-        result = second - first;
-        System.out.println("Result subtract: " + result);
-        result = second * first;
-        System.out.println("Result multiply: " + result);
-        result = second / first;
-        System.out.println("Result divide: " + result);
+        first += second;
+        System.out.println("Result sum: " + first);
+        second -= first;
+        System.out.println("Result subtract: " + second);
+        first *= second;
+        System.out.println("Result multiply: " + first);
+        second /= first;
+        System.out.println("Result divide: " + second);
     }
 }

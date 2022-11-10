9deed25266535cac36cee88dffd2f3925c589016
Denis Lazykin <dlazykin856@gmail.com>
2022-11-10 09:34:07 +0300

    9.2. Математические операторы и составные операторы присваивания [#504905]. Добавил класс FourDigitNumber

---
+++ src/main/java/ru/job4j/condition/FourDigitNumber.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+/**
+ * Класс в котором демонстрируется как разбить четырехзначное число на составляющие
+ */
+public class FourDigitNumber {
+    public static void main(String[] args) {
+        int i = 1234;
+        int thousands = i / 1000;
+        int hundreds = i % 1000 / 100;
+        int tens = i % 1000 % 100 / 10;
+        int single = i % 1000 % 100 % 10;
+        System.out.println("thousands: " + thousands
+        + ", hundreds : " + hundreds
+        + ", tens: " + tens
+        + ", single: " + single);
+    }
+}

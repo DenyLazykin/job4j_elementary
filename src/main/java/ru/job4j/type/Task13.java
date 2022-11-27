354a4fabacb67590ceb677d7e719022ef802b869
Denis Lazykin <dlazykin856@gmail.com>
2022-11-27 15:00:47 +0300

    13. Работа с действительными числами.Добавил описание задачи

--- src/main/java/ru/job4j/type/Task13.java
+++ src/main/java/ru/job4j/type/Task13.java
@@ -2,6 +2,11 @@ package ru.job4j.type;
 
 import java.util.Scanner;
 
+/**
+ * \
+ * Даны два действительных числа a и b,
+ * вывести их сумму, разность, произведение, частное - результат каждого вычисления вывести в новую строку.
+ */
 public class Task13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);

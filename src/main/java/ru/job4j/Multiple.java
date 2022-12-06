6c5811b8ad1670c1860508933988ed43d7f18f3d
Denis Lazykin <dlazykin856@gmail.com>
2022-12-06 09:23:57 +0300

    Написал javadoc для класса Multiple

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -1,5 +1,8 @@
 package ru.job4j;
 
+/**
+ * Выводимм таблицу результат умножения на экран
+ */
 public class Multiple {
     public static void main(String[] args) {
         System.out.println("1 * 1 = 1");

37fa70c90eefad332e9318e39896ba782a83e7db
Denis Lazykin <dlazykin856@gmail.com>
2022-12-14 18:07:35 +0300

    18. Простые геометрические вычисления

---
+++ src/main/java/ru/job4j/first/Task18.java
@@ -0,0 +1,25 @@
+package ru.job4j.first;
+
+import java.util.Scanner;
+
+/**
+ * Дана сторона квадрата a. Найти его периметр.
+ * Дан радиус окружности r. Найти длину окружности и площадь круга.
+ * Результат каждого вычисления выводите в новой строке с точностью до десятых.
+ */
+public class Task18 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double r  = input.nextDouble();
+        double squarePerimeter = a * 4;
+        String resultSquarePerimeter = String.format("%.1f", squarePerimeter);
+        double circleLength = 2 * Math.PI * r;
+        String resultCircleLength = String.format("%.1f", squarePerimeter);
+        double circleArea = Math.PI * Math.pow(r, 2);
+        String resultCircleArea = String.format("%.1f", squarePerimeter);
+        System.out.println(resultSquarePerimeter);
+        System.out.println(resultCircleLength);
+        System.out.println(resultCircleArea);
+    }
+}

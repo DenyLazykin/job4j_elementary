2c583d5731b4d5d8b71b94eea97ccdc57c6bd589
Denis Lazykin <dlazykin856@gmail.com>
2023-01-03 11:25:29 +0300

    4.1. Элементы интерфейса [#504948].Добавим в интерфейсы Func1, Func2 дефолтные методы funcMessage().

--- src/main/java/ru/job4j/polymorph/Func2.java
+++ src/main/java/ru/job4j/polymorph/Func2.java
@@ -4,4 +4,8 @@ public interface Func2 {
     default double func(double x, double y) {
         return x * x - y + 5;
     }
+
+    default void funcMessage() {
+        System.out.println("Сообщение из Func2");
+    }
 }
